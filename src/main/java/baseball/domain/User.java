96492df69169e91eec0fa8bd07e6260943cfe928
진우 <nice7677@naver.com>
2023-07-16 22:36:54 +0900

    refactor: 수정

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,8 +1,5 @@
 package baseball.domain;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class User {
 
     private final Numbers numbers;
--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,9 +1,19 @@
 package baseball.service;
 
+import baseball.code.Hint;
+import baseball.domain.Computer;
+import baseball.domain.User;
+
+import java.util.EnumMap;
+
 public class BaseballService {
 
-    public void validMatch() {
+    private static final int TREE_STRIKE = 3;
 
-    }
+//    public EnumMap<Hint, Integer> getMatchedHintCounts(Computer computer, User user) {
+//        EnumMap<Hint, Integer> hintCounts = new EnumMap<>(Hint.class);
+//
+//    }
+
 
 }
