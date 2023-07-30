01f55dc9e05b4ac1cadcc3cfe97c1b253e698b43
yuna <youna138@gmail.com>
2023-07-30 22:43:43 +0900

    숫자 validation 추가

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -5,15 +5,20 @@ import java.util.List;
 public class Numbers {
     private List<Integer> numbers;
 
-    private static final String INVALID_INPUT = "잘못된 입력값 입니다.";
+    private static final String INVALID_INPUT = "1부터 9까지 서로 다른 3자리의 수를 입력하세요.";
     private static final int MAX_LENGTH = 3;
 
     public Numbers(List<Integer> numbers){
-        if(numbers.size() != MAX_LENGTH) throw new IllegalArgumentException(INVALID_INPUT);
+        validationNumber(numbers);
         this.numbers = numbers;
     }
 
     public List<Integer> getNumber(){
         return numbers;
+    }
+
+    private void validationNumber(List<Integer> numbers){
+        if(numbers.size() != MAX_LENGTH) throw new IllegalArgumentException(INVALID_INPUT);
+        if(numbers.contains(0)) throw new IllegalArgumentException(INVALID_INPUT);
     }
 }
